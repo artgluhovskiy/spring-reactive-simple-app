d50936ce92eb460fe020599074606ef0856cd1bc
Artsiom Hlukhouski <gluhovskiy.art@gmail.com>
2018-10-23 18:24:48 +0300

    Add hello test controller.

--- src/main/java/org/art/web/app/ReactiveApplication.java
+++ src/main/java/org/art/web/app/ReactiveApplication.java
@@ -1,6 +1,5 @@
 package org.art.web.app;
 
-import org.art.web.app.webclient.GreetingWebClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +9,7 @@ public class ReactiveApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReactiveApplication.class, args);
 
-        GreetingWebClient gwc = new GreetingWebClient();
-        System.out.println(gwc.getResult());
+//        GreetingWebClient gwc = new GreetingWebClient();
+//        System.out.println(gwc.getResult());
     }
 }
---
+++ src/main/java/org/art/web/app/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package org.art.web.app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello.do", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello";
+    }
+}
